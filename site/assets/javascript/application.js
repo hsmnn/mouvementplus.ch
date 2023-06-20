eb4961b2262a6e5600eaddd3fb98d95ef7658ceb
hausmanne <54555072+hsmnn@users.noreply.github.com>
2023-06-20 14:59:22 +0200

    read more read less button added

--- site/assets/javascript/application.js
+++ site/assets/javascript/application.js
@@ -8,3 +8,19 @@ function nav(){
         window.addEventListener('scroll', stickyNav);
     })
 }
+
+function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.querySelector(".more");
+    var btnText = document.getElementById("readbtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Voir plus";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Voir moins";
+      moreText.style.display = "inline";
+    }
+  }
